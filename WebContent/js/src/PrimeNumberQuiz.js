bfdc76a0a2f86e3196b283f5835af16cd1b1f5f2
Rebecca Case <mrcase@yahoo.com>
2013-03-04 21:42:47 -0600

    has jquery tests

--- WebContent/js/src/PrimeNumberQuiz.js
+++ WebContent/js/src/PrimeNumberQuiz.js
@@ -1,10 +1,69 @@
 
 function PrimeNumberQuiz() {
+	var quizStarted = false;
+	var secondsElapsed = 1;
+	
+	var incrementSecondsElapsed = function() {
+		
+		$('#timeLeft').html(secondsElapsed);
+		secondsElapsed++;
+	};
+	
+	var endQuiz = function(intervalId) {
+		window.clearInterval(intervalId);
+		quizStarted = false;
+	};
+	
+	var resetPageState = function() {
+		$('#answers').empty();
+		$('#timeLeft').html("0");
+		
+		quizStarted = true;
+	};
+	
+	var handleSubmit = function() {
+		if(quizStarted) {
+			var submittedAnswer = $('#numberInput').val();
+			var answerClass = isPrime(submittedAnswer) ? 'correct' : 'incorrect';
+			
+			var displayAnswer = $('<div>').addClass(answerClass).append(submittedAnswer);
+			$('#answers').append(displayAnswer);
+			$('#numberInput').val('');
+		}
+	};
+	
+	
+	var runQuizTimer = function() {
+		var intervalId = window.setInterval(function(){incrementSecondsElapsed();},1000);
+		window.setTimeout(function(){endQuiz(intervalId);}, 15100);
+	};
+	
+	var isPrime = function(num) {
+        var result = true;
+        if (num !== 2) {
+            if (num % 2 == 0) {
+                result = false;
+            } else {
+                for (var x=3; x<=Math.sqrt(num); x+=2) {
+                    if (num % x == 0) result = false;
+                }
+            }
+        }
+        return result;
+    };
+	
 	var generateObj = function() {
 		return {
 			startQuiz: function(){
-				alert("hello world!");
+				resetPageState();
 				
+				$('#submitButton').click(handleSubmit);
+				
+				runQuizTimer();
+			},
+			
+			isQuizStarted: function(){
+				return quizStarted;
 			}
 		};
 		
--- WebContent/js/test/PrimeNumberQuizTest.js
+++ WebContent/js/test/PrimeNumberQuizTest.js
@@ -1,9 +1,149 @@
-module( "gold tests", {
+module( "Initialization Tests", {
     setup: function() {
-       // this.primeNumberQuiz = PrimeNumberQuiz();
+    	$('#qunit-fixture').append('<div id="answers">Yo yo yo</div>');
+    	$('#qunit-fixture').append('<div id="timeLeft">45</div>');
+        
+    	this.primeNumberQuiz = PrimeNumberQuiz();
     }
 });
 
-test("hello test", function(){
-	ok("hello");
+test("prime number quiz can be instantiated", function(){
+	ok(this.primeNumberQuiz);
+});
+
+test("quiz is not started before start button is pressed", function(){
+	ok(!this.primeNumberQuiz.isQuizStarted());
+});
+
+test("quiz is started after start button is pressed", function(){
+	this.primeNumberQuiz.startQuiz();
+	
+	ok(this.primeNumberQuiz.isQuizStarted());
+});
+
+test("answers div is cleared out when quiz is started", function(){
+	this.primeNumberQuiz.startQuiz();
+	
+	deepEqual("", $('#answers').html());
+});
+
+test("timer is reset to 0 when quiz is started", function(){
+	this.primeNumberQuiz.startQuiz();
+	
+	deepEqual("0", $('#timeLeft').html());
+});
+
+module( "Timer Tests", {
+    setup: function() {
+    	$('#qunit-fixture').append('<div id="answers">Yo yo yo</div>');
+    	$('#qunit-fixture').append('<div id="timeLeft">45</div>');
+        
+    	this.clock = sinon.useFakeTimers();
+    	this.primeNumberQuiz = PrimeNumberQuiz();
+    }
+});
+
+test("timer is 0 after quiz is started and less than one second has passed", function(){
+	this.primeNumberQuiz.startQuiz();
+	this.clock.tick(999);
+	
+	deepEqual("0", $('#timeLeft').html());
+});
+
+test("timer is incremented to 1 after quiz is started and one second has passed", function(){
+	this.primeNumberQuiz.startQuiz();
+	this.clock.tick(1001);
+	
+	deepEqual("1", $('#timeLeft').html());
+});
+
+test("timer is incremented to 2 after quiz is started and two seconds have passed", function(){
+	this.primeNumberQuiz.startQuiz();
+	this.clock.tick(2001);
+	
+	deepEqual("2", $('#timeLeft').html());
+});
+
+test("timer is incremented to 15 after quiz is started and 15 seconds have passed", function(){
+	this.primeNumberQuiz.startQuiz();
+	this.clock.tick(15001);
+	
+	deepEqual("15", $('#timeLeft').html());
+});
+
+test("timer is NOT incremented after quiz is started and more than 15 seconds have passed", function(){
+	this.primeNumberQuiz.startQuiz();
+	this.clock.tick(16000);
+	
+	deepEqual("15", $('#timeLeft').html());
+});
+
+test("quiz ends after 15 seconds have passed", function(){
+	this.primeNumberQuiz.startQuiz();
+	ok(this.primeNumberQuiz.isQuizStarted());
+	this.clock.tick(16000);
+	
+	ok(!this.primeNumberQuiz.isQuizStarted());
+});
+
+module( "Quiz Tests", {
+    setup: function() {
+    	$('#qunit-fixture').append('<div id="answers">Yo yo yo</div>');
+    	$('#qunit-fixture').append('<div id="timeLeft">45</div>');
+    	$('#qunit-fixture').append('<<input type="text" name="numberInput" id="numberInput">');
+    	$('#qunit-fixture').append('<input type="button" name="submitButton" id="submitButton" value="Submit">');
+        
+    	this.clock = sinon.useFakeTimers();
+    	this.primeNumberQuiz = PrimeNumberQuiz();
+    }
+});
+
+test("correct entry is added to answer area on submit after quiz is started", function(){
+	this.primeNumberQuiz.startQuiz();
+	
+	$('#numberInput').val("1");
+	$('#submitButton').click();
+	
+	deepEqual('<div class="correct">1</div>', $('#answers').html());
+});
+
+test("incorrect entry is added to answer area on submit after quiz is started", function(){
+	this.primeNumberQuiz.startQuiz();
+	
+	$('#numberInput').val("4");
+	$('#submitButton').click();
+	
+	deepEqual('<div class="incorrect">4</div>', $('#answers').html());
+});
+
+test("number entry area is cleared after a submit", function(){
+	this.primeNumberQuiz.startQuiz();
+	
+	$('#numberInput').val("4");
+	$('#submitButton').click();
+	
+	deepEqual($('#numberInput').val(), '');
+});
+
+test("answers are appended to previous answers", function(){
+	this.primeNumberQuiz.startQuiz();
+	
+	$('#numberInput').val("1");
+	$('#submitButton').click();
+	$('#numberInput').val("4");
+	$('#submitButton').click();
+	
+	ok($('#answers').html().indexOf('<div class="correct">1</div>') >= 0);
+	ok($('#answers').html().indexOf('<div class="incorrect">4</div>') >= 0);
+});
+
+test("answers are not added after 15 seconds", function(){
+	this.primeNumberQuiz.startQuiz();
+	
+	this.clock.tick(16000);
+	
+	$('#numberInput').val("1");
+	$('#submitButton').click();
+	
+	ok($('#answers').html().indexOf('<div class="correct">1</div>') < 0);
 });
